4d8e146b578b2568486357b154bee6f8662825ce
Jarom Roney <byutrueblue@gmail.com>
2018-02-14 22:35:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,13 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+function addElementToBeginningOfArray(array,element){
+  return array.unshift(element)
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array = array.unshif(element)
+}
+function addelementToEndOfArray(array,element){
+  return array.push(element)
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array = array.unshif(element)
+}

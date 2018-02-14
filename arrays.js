7be96f585e028f60ce1fd2eaa797bc6e57440993
Jarom Roney <byutrueblue@gmail.com>
2018-02-14 22:41:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,10 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array,index){
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
